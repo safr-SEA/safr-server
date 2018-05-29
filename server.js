44ad27e9e0785d55f479ea11a181e8f2a921a9cb
Karen Mac <karenbtlai@gmail.com>
2018-05-29 12:36:35 -0700

    create postgres database and store API data locally

--- server.js
+++ server.js
@@ -14,76 +14,68 @@ const CLIENT_URL = process.env.CLIENT_URL;
 
 const API_KEY = process.env.SOCRATA_KEY;
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client('postgres://localhost:5432/safr');
+client.connect();
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
 
 app.get('/', (req, res) => res.send(`It's ALLLLIIIIVEEE!!`));
 
+loadDB();
+
 app.get('/data/sea-gov', (req, res) => {
-    console.log('get');
     superagent
-        .get('https://data.seattle.gov/resource/y7pv-r3kh.json')
+    .get('https://data.seattle.gov/resource/y7pv-r3kh.json')
         .query({
-            '$where': "(offense_code like '12%' or offense_code like '13%' or offense_code like '16%' or offense_code like '21%' or offense_code like '35%') and date_reported > '2018-02-01T12:00:00'",
+            '$where': "(offense_code like '12%' or offense_code like '13%' or offense_code like '16%' or offense_code like '21%') and date_reported > '2018-02-01T12:00:00'",
             '$$app_token': `${API_KEY}`
-        })
-        .then(response => res.send(response.body))
+				})
+        .then(response => {
+					response.body.forEach((e) => {
+						let SQL = 'INSERT INTO crime_reports(offense_code, summarized_offense_description, date_reported, hundred_block_location, district_sector, zone_beat, longitude, latitude, location) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) ON CONFLICT DO NOTHING';
+						let values = [e.offense_code, e.summarized_offense_description, e.date_reported, e.hundred_block_location, e.district_sector, e.zone_beat, e.longitude, e.latitude, e.location];
+						client.query(SQL, values)
+							.catch(console.error);
+					})
+					res.send(response.body);
+				})
         .catch(console.error);
 });
 
-
-// function loadReports() {
-//     let SQL = 'SELECT COUNT(*) FROM crime_reports';
-//     client.query(SQL)
-//         .then( result => {
-//             if(!parseInt(result.rows[0].count)) {
-//                 fs.readFile('./')
-//             }
-
-//         })
-// }
-
 function loadDB() {
+		console.log('loadDB');
     client.query(`
     CREATE TABLE IF NOT EXISTS
     crime_reports (
-      report_id SERIAL PRIMARY KEY,
-      rms_cdw_id VARCHAR(30),
-      general_offense_number VARCHAR(30),
-      offense_code VARCHAR(10),
-      offense_code_extension VARCHAR(5),
-      offense_type VARCHAR(50),
-      summary_offense_code VARCHAR(10),
-      summarized_offense_description VARCHAR(50),
-      date_reported FLOATING TIMESTAMP,
-      occurred_date_or_date_range_start FLOATING TIMESTAMP,
-      occurred_date_range_end FLOATING TIMESTAMP,
+      report_id SERIAL PRIMARY KEY,      
+      offense_code VARCHAR(10),      
+			summarized_offense_description VARCHAR(50),
+			date_reported TIMESTAMPTZ,      
       hundred_block_location VARCHAR(50),
       district_sector VARCHAR(5),
-      zone_beat VARCHAR(5),
-      census_tract_2000 VARCHAR(15),
-      longitude DOUBLE,
-      latitude DOUBLE,
-      location VARCHAR(100),
-      month INTEGER,
-      year INTEGER,
+      zone_beat VARCHAR(5),			
+			longitude DOUBLE PRECISION,
+      latitude DOUBLE PRECISION,
+      location VARCHAR(100)			
     );`
     )
     .then(console.log)
-    .then(loadReports)
     .catch(console.error);
 }
-
-
-
-
-
-
-
-
 
 app.get('*', (request, response) => response.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
+//rms_cdw_id VARCHAR(30),
+//general_offense_number VARCHAR(30),
+//offense_code_extension VARCHAR(5),
+//offense_type VARCHAR(50),
+//summary_offense_code VARCHAR(10),
+//occurred_date_or_date_range_start TIMESTAMPTZ,
+//occurred_date_range_end TIMESTAMPTZ,
+//census_tract_2000 VARCHAR(15),
+//month INTEGER,
+//year INTEGER
+		
